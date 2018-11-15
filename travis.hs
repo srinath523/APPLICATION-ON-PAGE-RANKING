fcd51e69db71962cb1bc3c3c9e240fe7f03b3a0e
srinath523 <42550175+srinath523@users.noreply.github.com>
2018-11-15 21:44:01 +0530

    Add files via upload

---
+++ travis.hs
@@ -0,0 +1,20 @@
+
+import System.Time.Extra
+import System.Process.Extra
+import Control.Exception.Extra
+
+
+main :: IO ()
+main = do
+    let timed x = do
+        putStrLn $ "\n\nSearching for " ++ x
+        (time,_) <- duration $ system_ $ "hoogle +RTS -T -M1.8G -K1000K -RTS " ++ show x
+        putStrLn $ "Search " ++ show x ++ " took " ++ showDuration time
+        putStrLn "\n\n"
+    retry 3 $ timed "generate"
+    timed "test"
+    timed "map"
+    timed "map package:base"
+    timed ":: a -> b"
+    timed ":: ShakeOptions -> a"
+    retry 3 $ timed "generate --frege"
